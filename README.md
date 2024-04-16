97dccf90dfd9f639ed3c04fe6e622d0074238bb6
officialgio121 <officialgio121@gmail.com>
2024-04-16 12:08:06 -0500

    Added CosmosDB support to the Common library

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ Common libraries used by Play Economy services.
 
 ## Create and publish package
 ```powershell
-$version="1.0.5"
+$version="1.0.6"
 $owner="play-economy-microservices"
 $gh_pat="[PAT HERE]"
 
-dotnet pack src/Play.Common/ --configuration Release -p:PackageVersion=$version -p:RepositoryUrl=https://github.com/$owner/play.common -o ../
+dotnet pack src/Play.Common/ --configuration Release -p:PackageVersion=$version -p:RepositoryUrl=https://github.com/$owner/play.common -o ../packages
 
 dotnet nuget push ../packages/Play.Common.$version.nupkg --api-key $gh_pat --source "github"
 ```
--- src/Play.Common/Settings/MongoDBSettings.cs
+++ src/Play.Common/Settings/MongoDBSettings.cs
@@ -2,10 +2,22 @@ namespace Play.Common.Settings;
 
 public class MongoDbSettings
 {
+    private string connectionString;
+
     public string Host { get; init; }
 
     public int Port { get; init; }
 
-    public string ConnectionString => $"mongodb://{Host}:{Port}";
+    /// <summary>
+    /// CosmosDB and MongoDB connection string.
+    /// </summary>
+    public string ConnectionString
+    {
+        get
+        {
+            return string.IsNullOrWhiteSpace(connectionString)
+                ? $"mongodb://{Host}:{Port}" : connectionString;
+        }
+        init { connectionString = value; }
+    }
 }
-
